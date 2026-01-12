ba7d9025aa184fa57878ed44088f59515ad784c9
smartboy84 <www.hamdanmahmood12@gmail.com>
2026-01-13 02:14:37 +0800

    clarify

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,11 @@ pub type ApiBackendResult<T, C> = Result<T, ApiBackendError<C>>;
 
 // hide this to not confuse user - user must then implement either ConstServer or DynamicServer to get this trait (due to blanket impls later)
 pub trait Server {}
-pub trait ConstServer {
+pub trait ConstServer: Server {
     const ROOT: &str;
 }
 
-pub trait DynamicServer {
+pub trait DynamicServer: Server {
     fn get_root(&self) -> &str;
 }
 
