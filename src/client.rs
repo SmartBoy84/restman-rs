eb517fb9b42831c64364eab2e36afd6ff2a77a91
smartboy84 <www.hamdanmahmood12@gmail.com>
2026-01-29 13:42:28 +0800

    correct naming

--- src/client.rs
+++ src/client.rs
@@ -8,15 +8,15 @@ use crate::{
 pub const AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0";
 
 // split server and client config into separate traits to allow backend to separate multiple servers
-pub trait ApiClient<C: Server> {}
+pub trait ApiClientServer<C: Server> {}
 
 pub trait ApiClientBackend<C: ApiHttpClient> {
     fn token(&self) -> &str;
     fn backend(&self) -> &C;
 }
 
-pub trait ApiClientBackendInner<C: ApiHttpClient, S: Server>:
-    ApiClient<S> + ApiClientBackend<C>
+pub trait ApiClient<C: ApiHttpClient, S: Server>:
+    ApiClientServer<S> + ApiClientBackend<C>
 {
     fn request<P: Endpoint<Payload = (), Ser = S>, R: ValidRequest<P>>(
         &self,
@@ -56,13 +56,13 @@ pub trait ApiClientBackendInner<C: ApiHttpClient, S: Server>:
     }
 }
 // access only permitted, if both specified!
-impl<C: ApiHttpClient, S: Server, T: ApiClient<S> + ApiClientBackend<C>> ApiClientBackendInner<C, S>
+impl<C: ApiHttpClient, S: Server, T: ApiClientServer<S> + ApiClientBackend<C>> ApiClient<C, S>
     for T
 {
 }
 
 // seal this trait - external users shouldn't be able to see it
-trait InnerGetter<C: ApiHttpClient, S: Server>: ApiClientBackendInner<C, S> {
+trait InnerGetter<C: ApiHttpClient, S: Server>: ApiClient<C, S> {
     // FUCK, you can set bounds on associated types?! This simplifies so much shit
     /*
     enforce that the method is one that implements the getter trait for our client
@@ -93,4 +93,4 @@ trait InnerGetter<C: ApiHttpClient, S: Server>: ApiClientBackendInner<C, S> {
         Ok(serde_json::from_reader(self.inner_raw_request(r, p)?)?)
     }
 }
-impl<C: ApiHttpClient, S: Server, T: ApiClientBackendInner<C, S> + ?Sized> InnerGetter<C, S> for T {}
+impl<C: ApiHttpClient, S: Server, T: ApiClient<C, S> + ?Sized> InnerGetter<C, S> for T {}
